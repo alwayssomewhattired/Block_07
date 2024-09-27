c2bebcb408889800e9d1d13f1d22ff328d4ed85f
Zachary Malinka <132944209+alwayssomewhattired@users.noreply.github.com>
2024-09-26 20:16:03 -0400

    Add files via upload

---
+++ stuff.js
@@ -0,0 +1,26 @@
+// 1. 3 variables
+// 2. Each variable is equal to a part of the combination lock
+// 3. Each variable needs to involve a different math approach
+// 4. Output, in console log, the string You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// 5. Output, in console log, the variables
+
+// Main String
+const rosetta_stone =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(rosetta_stone);
+
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+
+// First Variable
+let zonk1 = 5 + 5;
+alert(zonk1);
+
+// Second Variable
+let zonk2 = 10 * 4;
+alert(zonk2);
+
+// Third Variable
+let zonk3 = 40 - 1;
+alert(zonk3);
